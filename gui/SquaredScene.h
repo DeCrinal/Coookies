09bc6a1a5465b8d38a548daa948c1095673e1ec8
Kuzart <kuznecov@imail.coresar.ru>
2024-08-12 21:22:16 +0300

    Еще правки форматирования

--- gui/SquaredScene.h
+++ gui/SquaredScene.h
@@ -9,17 +9,20 @@
 using std::vector;
 class SqScene:public QGraphicsScene{
     Q_OBJECT
+
 private:
     void createBoards();
+
 public slots:
-    void is_collision(QGraphicsItem*);
+    void is_collision(QGraphicsItem* item);
+
 public:
-    vector<CircleCoin*>coins;
-    vector<SquaredEnemy*>enemies;
-    vector<FinishPoint*>finish_points;
-    vector<vector<int>>boards;
-    SquaredHero*squaredHero;
-    SqScene(QObject*parent=nullptr);
+    vector<CircleCoin*> coins;
+    vector<SquaredEnemy*> enemies;
+    vector<FinishPoint*> finish_points;
+    vector<vector<int>> boards;
+    SquaredHero* squaredHero;
+    SqScene(QObject*parent = nullptr);
     void setVector(const QString& vector_from);
     ~SqScene();
     void get_level_data(int choosed_lvl);
